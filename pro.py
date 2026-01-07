82ca3d8b54665d42c3824a79fd740724fe1844a8
cherukuriyogini <cherukuriyogini@gmail.com>
2026-01-07 15:56:18 +0530

    change

---
+++ pro.py
@@ -0,0 +1,22 @@
+#Example 1:
+
+#Input: num = "52"
+  #Output: "5"
+#Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+#Example 2:
+
+#Input: num = "4206"
+#Output: ""
+#Explanation: There are no odd numbers in "4206".
+#Example 3:
+
+#Input: num = "35427"
+#Output: "35427"
+#Explanation: "35427" is already an odd number.
+
+#simple answer code in python code
+def largestOddNumber(num):
+    for i in range(len(num)-1,-1,-1):
+        if int(num[i])%2==1:
+            return num[:i+1]
+    return ""
